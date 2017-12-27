a0f262cf2c9ced05c8b94a49945f85e257980be2
fengjiran <fengjiran@foxmail.com>
2017-12-27 08:41:04 +0800

    modify

--- perceptron/binary_perceptron.py
+++ perceptron/binary_perceptron.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 import random
 import cv2
 import pandas as pd
@@ -6,6 +8,10 @@ import time
 
 from sklearn.cross_validation import train_test_split
 from sklearn.metrics import accuracy_score
+
+feature_length = 324
+object_num = 0
+study_total = 10000
 
 
 def get_hog_feature(trainset):
@@ -28,42 +34,48 @@ class Perceptron(object):
 
     def __init__(self):
         self.lr = 0.00001
-        self.max_iteration = 5000
         self.w = None
+        self.b = None
 
-    def predict_(self, x):
-        wx = sum([self.w[j] * x[j] for j in range(len(self.w))])
-        return int(wx > 0)
+    def train(self, trainset, labels):
+        trainset_size = len(labels)
+        self.w = np.zeros((feature_length))
+        self.b = 0.0
 
-    def train(self, features, labels):
-        self.w = [0.0] * (len(features[0]) + 1)
-        correct_count = 0
-        iters = 0
+        study_count = 0
+        nochange_count = 0
+        nochange_upper_limit = 100000
 
-        while iters < self.max_iteration:
-            index = random.randint(0, len(labels) - 1)
-            x = list(features[index])
-            x.append(1.0)
-            y = 2 * labels[index] - 1
-            wx = sum([self.w[j] * x[j] for j in range(len(self.w))])
+        while True:
+            nochange_count += 1
+            if nochange_count > nochange_upper_limit:
+                break
 
-            if wx * y > 0:
-                correct_count += 1
-                if correct_count > self.max_iteration:
+            index = random.randint(0, trainset_size - 1)
+            img = trainset[index]
+            label = labels[index]
+
+            yi = int(label != object_num) * 2 - 1
+            result = yi * (np.dot(img, self.w) + self.b)
+
+            if result < 0:
+                img = np.reshape(trainset[index], (feature_length, 1))
+                self.w += img * yi * self.lr
+                self.b += yi * self.lr
+
+                study_count += 1
+                if study_count > study_total:
                     break
-                continue
+                nochange_count = 0
 
-            for i in range(len(self.w)):
-                self.w[i] += self.lr * (y * x[i])
+    def predict(self, testset):
+        predict = []
+        for img in testset:
+            result = np.dot(img, self.w) + self.b
+            result = result > 0
+            predict.append(result)
 
-    def predict(self, features):
-        labels = []
-        for feature in features:
-            x = list(feature)
-            x.append(1.0)
-            labels.append(self.predict_(x))
-
-        return labels
+        return np.array(predict)
 
 
 if __name__ == '__main__':
